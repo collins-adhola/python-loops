68a5c2e0ae46d2f76ef865b022bc7daa3fd6a89d
collins_adhola <coadhola@gmail.com>
2021-04-26 19:21:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#====For in loop====
+# students = ['Jane','Andria','luke']
+# for x in students:
+#   print('hi', x)
+
+#============
+# print(range(10))
+# print(list(range(10)))
+
+# print(list(range(3,7)))
+
+# print(list(range(2,14,3)))
+
+# for i in range(2,8):
+#   for j in range(i):
+#     print('*', end ='')
+#   print()
+
+  #====While loop===
+  # i = 1
+  # while i < 11:
+  #   print (i)
+  #   i += 1
+
+  #===Break statements===
+i = 1
+while i < 11:
+    print (i)
+    if i == 3:
+        break
+    i += 1
